440f76da80abd133e882a1b6de25bb7c0ab8204a
Matthew Young <luckymatty@protonmail.com>
2023-10-17 22:20:16 -0700

    Initial Commit

---
+++ PureSwiftUILearning/PureSwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureSwiftUILearningApp.swift
+//  PureSwiftUILearning
+//
+//  Created by Matthew Young on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PureSwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
